1aa0430ed73ba9e16287ce55bc0a2f58014c01cb
Oleksandr Opanasevych <karabas2903@gmail.com>
2020-03-23 23:22:01 +0200

    to heroku

--- ebloid_bot.py
+++ ebloid_bot.py
@@ -1,6 +1,7 @@
 import telebot as tbot
 import random as rm
 import os
+from telebot import types
 
 TOKEN = os.environ['BOT_TOKEN']
 API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN
@@ -10,8 +11,8 @@ bot = tbot.TeleBot(TOKEN)
 @bot.inline_handler(lambda query: query.query == 'text')
 def query_text(inline_query):
     try:
-        r = tbot.types.InlineQueryResultArticle('1', 'Result1', tbot.types.InputTextMessageContent('1'))
-        r2 = tbot.types.InlineQueryResultArticle('2', 'Result2', tbot.types.InputTextMessageContent('hi'))
+        r = types.InlineQueryResultArticle('1', 'Result1', types.InputTextMessageContent('1'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('hi'))
         bot.answer_inline_query(inline_query.id, (r, r2))
     except Exception as e:
         print(e)
@@ -20,7 +21,7 @@ def query_text(inline_query):
 @bot.inline_handler(lambda query: len(query.query) == 0)
 def default_query(inline_query):
     try:
-        r = tbot.types.InlineQueryResultArticle('1', 'Google', tbot.types.InputTextMessageContent('google'), url='https://google.com', hide_url=True)
+        r = types.InlineQueryResultArticle('1', 'Google', types.InputTextMessageContent('google'), url='https://google.com', hide_url=True)
         bot.answer_inline_query(inline_query.id, [r])
     except Exception as e:
         print(e)
@@ -45,7 +46,7 @@ def start_msg(message):
 def answer(message):
     if message.text.lower() == 'ти уйобище':
         bot.send_message(message.chat.id, 'мамка твоя уйобище')
-    if message.text.lower() == 'гендзюцу':
+    '''if message.text.lower() == 'гендзюцу':
         with open('papi4_dota/gendzhycy.wav', 'rb') as f:
             bot.send_voice(message.chat.id, f, reply_to_message_id=message.chat.id)
     if message.text.lower() == 'на':
@@ -59,7 +60,7 @@ def answer(message):
             bot.send_voice(message.chat.id, f, reply_to_message_id=message.chat.id)
     if message.text.lower() == 'соль':
         with open('papi4_dota/V SOLYANOGO.wav', 'rb') as f:
-            bot.send_voice(message.chat.id, f, reply_to_message_id=message.chat.id)
+            bot.send_voice(message.chat.id, f, reply_to_message_id=message.chat.id)'''
     if message.text.lower() == 'бондар':
         with open('TY SHO.ogg', 'rb') as f:
             bot.send_voice(message.chat.id, f, reply_to_message_id=message.chat.id)
